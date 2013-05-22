eae9aac6df4e182b4a7f806b68d78c38be94e4b3
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2013-05-22 19:59:27 +0800

    Begin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Overheard::Application.config.secret_token = '30de6063f292193100ab3b4f524acdcb5ed05956ec3aa0bd39ee3c7fd46f8d5ca2ca781177a79cacd882cf8e2ca5e031db940118480be5643576e346a61b9689'
